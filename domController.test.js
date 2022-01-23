5177c115446ecb6d0c7bce057b098f0ba27fa3fb
mar1n <30738216+mar1n@users.noreply.github.com>
2022-01-23 18:12:40 +0000

    button test exist

--- domController.test.js
+++ domController.test.js
@@ -15,7 +15,7 @@ describe("form", () => {
 describe("input", () => {
     test("input exist in dom structure", () => {
         const inputList = document.getElementById("input-container");
-        expect(inputList.childNodes).toHaveLength(2);
+        expect(inputList.childNodes).toHaveLength(3);
 
         const nodesName = Array.from(inputList.childNodes).map(
             node => ({ element: node.nodeName, type: node.type})
@@ -26,5 +26,14 @@ describe("input", () => {
 
         expect(nodesName[0].type).toBe("email");
         expect(nodesName[1].type).toBe("password");
+    });
+});
+
+
+describe("button", () => {
+    test("button exist in dom structure", () => {
+        const button = document.getElementById("submit-button");
+
+        expect(button.tagName).toBe("BUTTON");
     });
 });
