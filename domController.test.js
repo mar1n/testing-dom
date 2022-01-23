26cda2c77418524f42fde5c8bfc078baea8f3a8b
mar1n <30738216+mar1n@users.noreply.github.com>
2022-01-23 17:56:50 +0000

    form exsist and inputs types

--- domController.test.js
+++ domController.test.js
@@ -3,11 +3,28 @@ const initialHtml = fs.readFileSync("index.html");
 
 beforeEach(() => {
     document.body.innerHTML = initialHtml;
-})
+});
+
+describe("form", () => {
+    test("form exist in dom structure", () => {
+        const form = document.getElementById("input-container");
+        expect(form.tagName).toBe("FORM");
+    });
+});
 
 describe("input", () => {
     test("input exist in dom structure", () => {
         const inputList = document.getElementById("input-container");
         expect(inputList.childNodes).toHaveLength(2);
-    })
+
+        const nodesName = Array.from(inputList.childNodes).map(
+            node => ({ element: node.nodeName, type: node.type})
+        );
+
+        expect(nodesName[0].element).toBe("INPUT");
+        expect(nodesName[1].element).toBe("INPUT");
+
+        expect(nodesName[0].type).toBe("email");
+        expect(nodesName[1].type).toBe("password");
+    });
 });
