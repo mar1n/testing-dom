b2c12e33dfc9b34f92cc9a4fcb291879ae36911c
mar1n <30738216+mar1n@users.noreply.github.com>
2022-01-29 22:54:26 +0000

    test green, submit button is disabled

--- domController.js
+++ domController.js
@@ -1,27 +1,41 @@
 const { userRegister, state } = require("./userController");
 
 function addNewUser(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const { email, password } = event.target.elements;
-    userRegister(email.value, password.value);
+  const { email, password } = event.target.elements;
+  userRegister(email.value, password.value);
 }
 
 const validEMail = (email) => /^\w+@\w+[.]\w+$/.test(email);
 
-const handleAddEmail = (event) => {
-    const emailValue = event.target.value;
+const checkFormValues = () => {
+  const email = document.querySelector(`input[name="email"]`).value;
+  const password = document.querySelector(`input[name="password"]`).value;
+console.log('email', email);
+  const emailIsEmpty = email === "";
+  const emialIsInvaild = !validEMail(email);
+  console.log('emailIsInvalid', emialIsInvaild);
+  const passwordIsEmpty = password === "";
 
-    const errorMSg = window.document.getElementById("error-email");
+  const errorMSg = window.document.getElementById("error-email");
 
-    if(emailValue === "") {
-        errorMSg.innerHTML = "";
-    } else if(!validEMail(emailValue)) {
-        errorMSg.innerHTML = "Email is invalid!"
-    } else {
-        errorMSg.innerHTML = `${emailValue} is valid format.`
-    }
+  if (emailIsEmpty) {
+    errorMSg.innerHTML = "";
+  } else if (emialIsInvaild) {
+    errorMSg.innerHTML = "Email is invalid!";
+  } else {
+    errorMSg.innerHTML = `${email} is valid format.`;
+  }
 
-}
+  const submitButton = document.querySelector(`button[type="submit"]`);
+  if (emailIsEmpty || emialIsInvaild || passwordIsEmpty) {
+    submitButton.disabled = true;
+    console.log('button true');
+  } else {
+    console.log('button false');
+    submitButton.disabled = false;
+  }
+};
 
-module.exports = { addNewUser, handleAddEmail }
+module.exports = { addNewUser, checkFormValues };
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const { addNewUser, handleAddEmail } = require("./domController");
+const { addNewUser, checkFormValues } = require("./domController");
 
 const form = document.getElementById("input-container");
 form.addEventListener("submit", addNewUser);
-const emailInput = document.querySelector(`input[name="email"]`);
-emailInput.addEventListener("input", handleAddEmail);
+form.addEventListener("input", checkFormValues);
+
+checkFormValues();
